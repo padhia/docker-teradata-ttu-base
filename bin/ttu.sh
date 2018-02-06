d4f535992cef0c31c2897d21b5e271addff65a83
Paresh Adhia <paresh_adhia@yahoo.com>
2018-02-06 11:30:52 -0500

    FIX: arguments were being dropped. Allow mount path to be overriden with ENVVAR

--- bin/ttu.sh
+++ bin/ttu.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
 
-docker run -i --rm --net=host -v $HOME:$HOME -v /tmp:/tmp -e "HOME=$HOME" ttu "$(basename $0)" "$@"
+CMD="$(basename $0)"
+
+# set TTUMOUNT variable to mount a host path, for example loading data. Defaults to $HOME
+if [ -z "$TTUMOUNT" ]; then
+    TTUMOUNT="$HOME"
+fi
+
+docker run -i \
+	--rm \
+	-h ttu \
+	--mount type=bind,source="$TTUMOUNT",target="$TTUMOUNT" \
+	--net=host \
+	--security-opt seccomp:unconfined \
+	--name docker-ttu \
+	ttu /bin/sh -c "cd $PWD;$CMD \"\$@\"" "$CMD" "$@"
